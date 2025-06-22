974b62ac97393dde884f7d25547fa514d5cb6179
Nay Zin Min Lwin <nayzinminlwin22@gmail.com>
2025-06-22 23:11:51 +0800

    #2 . Got barebone CDK project with fine tuned AWS CLI Validation.

--- lib/e_cv_project-stack.ts
+++ lib/e_cv_project-stack.ts
@@ -1,4 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
+import { SqsDestination } from 'aws-cdk-lib/aws-s3-notifications';
+import { Bucket, CfnBucket, EventType } from 'aws-cdk-lib/aws-s3';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -12,5 +15,22 @@ export class ECvProjectStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'ECvProjectQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    const level1S3Bucket = new CfnBucket(this,"MyFirstLevel1ConstructBucket",{
+      versioningConfiguration : {
+        status : "Enabled"
+      }
+    });
+
+    const level2S3Bucket = new Bucket(this,"MyFirstLevel2ConstructBucket",{
+      bucketName : "myfirstlevel2bucketirl",
+      versioned : true
+    });
+
+    const myQueue = new Queue(this,"MyQueue",{
+      queueName : "MyQueue",
+    });
+
+    level2S3Bucket.addEventNotification(EventType.OBJECT_CREATED,new SqsDestination(myQueue));
   }
 }
