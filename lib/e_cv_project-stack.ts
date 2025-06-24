9e7e97a186124f21a4fe11d03e5afe001d994cd5
Nay Zin Min Lwin <nayzinminlwin22@gmail.com>
2025-06-25 00:52:39 +0800

    #3 About to start lambda function scheduling.

--- lib/e_cv_project-stack.ts
+++ lib/e_cv_project-stack.ts
@@ -17,21 +17,53 @@ export class ECvProjectStack extends cdk.Stack {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
 
-    const level1S3Bucket = new CfnBucket(this,"MyFirstLevel1ConstructBucket",{
-      versioningConfiguration : {
-        status : "Enabled"
-      }
-    });
+    // Creating a level1 bucket which i need to identify all by manual.
+    // const level1S3Bucket = new CfnBucket(this,"MyFirstLevel1ConstructBucket",{
+    //   versioningConfiguration : {
+    //     status : "Enabled"
+    //   }
+    // });
 
-    const level2S3Bucket = new Bucket(this,"MyFirstLevel2ConstructBucket",{
-      bucketName : "myfirstlevel2bucketirl",
-      versioned : true
-    });
+    // // level2 bucket (the sweet spot) where everything necessary is auto. 
+    // const level2S3Bucket = new Bucket(this,"MyFirstLevel2ConstructBucket",{
+    //   bucketName : "myfirstlevel2bucketirl",
+    //   versioned : true,
+    // });
 
+    const bucket = new Bucket(this, "ReportBucket", {
+
+      bucketName : "report_bucket",
+
+      // to delete all inside the bucket after cdk destroy
+      removalPolicy : cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects : true,
+      // Good practice for experiment data
+
+    })
+
+
+    // Creating a sqsQueue
     const myQueue = new Queue(this,"MyQueue",{
       queueName : "MyQueue",
     });
 
-    level2S3Bucket.addEventNotification(EventType.OBJECT_CREATED,new SqsDestination(myQueue));
+    // Creating a lambda function
+    const fn = new lambda.Function(this,"FetcherFunction",{
+      runtime : lambda.Runtime.NODEJS_18_X, // use nodeJS 18x runtime
+      handler : 'index.handler',            // point to export.handler in index.js
+      code : lambda.Code.fromAsset('lambda'), // package up everything in ./lambda/
+      environment : {
+        // pass the bucket name into the function as an environment variable
+        BUCKET_NAME : bucket.bucketName,
+      },
+    });
+
+    // granting lambda function to put data into bucket
+    bucket.grantPut(fn);
+
+    
+
+    // // Once the s3 object is created, add the event notification to queue
+    // level2S3Bucket.addEventNotification(EventType.OBJECT_CREATED,new SqsDestination(myQueue));
   }
 }
